b3499d8d9a2d6b312e97f72562502805be51746a
Juan Osorio <96839034+JuanOsorioa@users.noreply.github.com>
2024-02-10 16:03:52 +0000

    Realizé mi primer programa orientado a objetos

--- Clase1/main.py
+++ Clase1/main.py
@@ -1,1 +1,12 @@
+class MotorElectrico:
+    potencia = None
+    def encender(self):
+        print("Encendiendo el motor electrico")
+
+    def __init__(self):
+        pass
+
+objeto = MotorElectrico()
+objeto.potencia = 1000
+objeto.encender()
 print("Primer codigo en python")
